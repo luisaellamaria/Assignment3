b543cc7128a59fa23c966d4f2bfbb41b88964196
luisaella <luisaella.mueller@student.unisg.ch>
2023-10-22 16:43:46 +0200

    Added Task 1

--- tasks/app/src/main/java/com/assignment3/spark/WordCount.java
+++ tasks/app/src/main/java/com/assignment3/spark/WordCount.java
@@ -1,19 +1,16 @@
 package com.assignment3.spark;
 
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path; // Correct import for Path
 import java.util.Arrays;
 import java.util.Iterator;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.*;
 import org.apache.spark.api.java.function.*;
-import org.apache.spark.SparkFiles;
-import java.util.List;
-import java.util.ArrayList;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
+
 
 import scala.Tuple2;
-
-import org.apache.spark.sql.Dataset;
 
 public class WordCount {
 
@@ -21,32 +18,67 @@ public class WordCount {
     {
         @Override
         public Iterator<String> call(String s) {
-            /*
-             * add your code to filter words
-             */
-            String[] subStrings = s.split("\\s+");
+            s = s.replaceAll("[^a-zA-Z\\s]", ""); // Remove non-alphabetic characters
+            String[] subStrings = s.toLowerCase().split("\\s+");
             return Arrays.asList(subStrings).iterator();
         }
+
 
     }
 
     public static void main(String[] args) {
-        String textFilePath = "input/pigs.txt"; // update to HDFS url for task2
-        SparkConf conf = new SparkConf().setAppName("WordCountWithSpark").setMaster("local[*]"); // task2: update the setMaster with your cluster master URL for executing this code on the cluster
-        JavaSparkContext sparkContext =  new JavaSparkContext(conf);
+        // Define the input file path
+        String textFilePath = "input/pigs.txt";
+
+        // Set up Spark configuration and context
+        SparkConf conf = new SparkConf().setAppName("WordCountWithSpark").setMaster("local[*]");
+        JavaSparkContext sparkContext = new JavaSparkContext(conf);
+
+        // Load the input text file into an RDD
         JavaRDD<String> textFile = sparkContext.textFile(textFilePath);
+
+        // Use the Filter class to split the text into words
         JavaRDD<String> words = textFile.flatMap(new Filter());
 
-        /*
-         * add your code for key value mapping
-         *
-         * add your code to perform reduce on the given key value pairs
-         *
-         * print the word count and save the output in the format, e.g.,(in:15) to an 'output' folder (on HDFS for task 2)
-         * try to consolidate your output into single text file if you want to check your output against the given sample output
-         */
+        // Map each word to a tuple (word, 1)
+        JavaPairRDD<String, Integer> counts = words.mapToPair(
+                new PairFunction<String, String, Integer>() {
+                    @Override
+                    public Tuple2<String, Integer> call(String s) {
+                        return new Tuple2<String, Integer>(s, 1);
+                    }
+                }
+        );
+
+        // Reduce the tuples by key to sum up the frequencies
+        JavaPairRDD<String, Integer> reducedCounts = counts.reduceByKey(
+                new Function2<Integer, Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer a, Integer b) {
+                        return a + b;
+                    }
+                }
+        );
+        // Save the result to an output directory. If output directory exists, delete it first.
+        String outputPath = "output";
+        reducedCounts.saveAsTextFile(outputPath);
+
+        // Consolidate output into a single file. This ensures only one part file is generated.
+        JavaRDD<String> consolidatedOutput = sparkContext.textFile(outputPath + "/*"); // read all parts
+        String consolidatedOutputPath = "consolidatedOutput";
+        consolidatedOutput.coalesce(1).saveAsTextFile(consolidatedOutputPath);
+
+        // Rename the file
+        try {
+            FileSystem fs = FileSystem.get(sparkContext.hadoopConfiguration());
+            Path oldPath = new Path(consolidatedOutputPath + "/part-00000");
+            Path newPath = new Path(consolidatedOutputPath + "/output-task1.txt");
+            fs.rename(oldPath, newPath);
+        } catch (Exception e) {
+            System.err.println("Error renaming the file: " + e); // replaced logger with a print statement
+        }
+        // Close the Spark context
         sparkContext.stop();
         sparkContext.close();
     }
 }
-
