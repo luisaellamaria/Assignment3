a8ea468547e987e4eebe21003ffdab10f85a8359
EmmaKozmer <emma.kozmer@student.unisg.ch>
2023-10-23 15:57:19 +0200

    updated task 2

--- tasks/app/src/main/java/com/assignment3/spark/WordCount.java
+++ tasks/app/src/main/java/com/assignment3/spark/WordCount.java
@@ -1,19 +1,16 @@
 package com.assignment3.spark;
 
 import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path; // Correct import for Path
+import org.apache.hadoop.fs.Path;
 import java.util.Arrays;
 import java.util.Iterator;
-
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.*;
 import org.apache.spark.api.java.function.*;
-
-
 import scala.Tuple2;
 
-public class WordCount {
 
+public class WordCount {
     static class Filter implements FlatMapFunction<String, String>
     {
         @Override
@@ -22,33 +19,28 @@ public class WordCount {
             String[] subStrings = s.toLowerCase().split("\\s+");
             return Arrays.asList(subStrings).iterator();
         }
-
-
     }
 
-
-
     public static void main(String[] args) {
-        String textFilePath = "input/pigs.txt";
-
-        System.out.println("Hello World from Spark! :)");
+        String textFilePath = "input/pigs.txt"; // input file path
 
         SparkConf conf = new SparkConf().setAppName("WordCountWithSpark").setMaster("local[*]");
         JavaSparkContext sparkContext = new JavaSparkContext(conf);
 
         JavaRDD<String> textFile = sparkContext.textFile(textFilePath);
-
         JavaRDD<String> words = textFile.flatMap(new Filter());
 
+        // Map each word to a tuple of (word, 1)
         JavaPairRDD<String, Integer> counts = words.mapToPair(
                 s -> new Tuple2<>(s, 1)
         );
 
+        // Reduce the tuples by key (word) and sum the values (counts)
         JavaPairRDD<String, Integer> reducedCounts = counts.reduceByKey(
                 Integer::sum
         );
 
-        String outputPath = "output";
+        String outputPath = "output"; // output directory path
 
         // Check and delete the output directory if it exists
         try {
@@ -62,14 +54,15 @@ public class WordCount {
             System.err.println("Error checking/deleting the output directory: " + e);
         }
 
+        // Save the output to the output directory
         reducedCounts.saveAsTextFile(outputPath);
 
         // Consolidate output into a single file.
         JavaRDD<String> consolidatedOutput = sparkContext.textFile(outputPath + "/*");
-        String consolidatedOutputPath = outputPath + "/tempConsolidated";
+        String consolidatedOutputPath = outputPath + "/tempConsolidated"; // temporary consolidated output directory path
         consolidatedOutput.coalesce(1).saveAsTextFile(consolidatedOutputPath);
 
-        // .Rename and move the consolidated file back to the original output directory.
+        // Rename and move the consolidated file back to the original output directory.
         try {
             FileSystem fs = FileSystem.get(sparkContext.hadoopConfiguration());
             Path oldPath = new Path(consolidatedOutputPath + "/part-00000");
